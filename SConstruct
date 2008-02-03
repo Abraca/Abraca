e1836e0e224a38312299ac612b46d4cf0e1c3d4c
Daniel Svensson <dsvensson@gmail.com>
2008-02-03 20:17:28 +0100

    OTHER: Break out a lot of crap unrelated to the actual build from SConstruct.

--- SConstruct
+++ SConstruct
@@ -1,86 +1,25 @@
-import SCons
+from abracaenv import AbracaEnvironment
 
-def CheckPkgConfig(ctx):
-	ctx.Message('Checking for pkg-config... ')
-	exit_code, output = ctx.TryAction('pkg-config --version')
-	ctx.Result(exit_code)
-	return exit_code
-
-def CheckPkg(ctx, name):
-	ctx.Message('Checking for %s... ' % name)
-	exit_code, output = ctx.TryAction('pkg-config --exists \'%s\'' % name)
-	ctx.Result(exit_code)
-	return exit_code
-
-def CheckGCC(ctx):
-	ctx.Message('Checking for compiler... ')
-	code = 'int main (int argc, char **argv) { return 0; }'
-	exit_code = ctx.TryCompile(code, '.c')
-	ctx.Result(exit_code)
-	return exit_code
-
-def CheckVala(ctx):
-	ctx.Message('Checking for valac... ')
-	code = 'public class Test { public static void main (string[] args) { } }'
-	exit_code = ctx.TryBuild(ctx.env.Vala, code, '.vala')
-	ctx.Result(exit_code)
-	return exit_code
-
-env = Environment()
-
-# Oldest usable SCons version.
-env.EnsureSConsVersion(0, 97)
-
-# Load the custom vala builder
-env.Tool('vala', toolpath=['scons-tools'])
-
-# Add some build options
-opts = Options(['.scons_options'])
-opts.AddOptions(
-	BoolOption('silent', 'build silently', True),
-	BoolOption('debug', 'build debug variant', True),
-	PathOption('prefix', 'install prefix', '/usr/local'),
-)
-opts.Update(env)
-opts.Save('.scons_options', env)
-
-env.Help(opts.GenerateHelpText(env))
-
-# Hide compiler command line if silent mode on
-if env['silent']:
-	env['VALADEFINESCOMSTR'] = '   Defines: $TARGET'
-	env['VALACOMSTR']        = 'Generating: $TARGETS'
-	env['CCCOMSTR']          = '  Building: $TARGET'
-	env['LINKCOMSTR']        = '   Linking: $TARGET'
+env = AbracaEnvironment()
 
 env.Append(VALAPKGPATH = ['vapi'])
 
-conf = Configure(env, custom_tests = {
-	'CheckPkgConfig' : CheckPkgConfig,
-	'CheckPkg' : CheckPkg,
-	'CheckVala' : CheckVala,
-	'CheckGCC' : CheckGCC,
-})
-
-if not conf.CheckVala():
-	raise SCons.Errors.UserError(pkg + ' required to build Abraca')
-if not conf.CheckGCC():
-	raise SCons.Errors.UserError(pkg + ' required to build Abraca')
-if not conf.CheckPkgConfig():
-	raise SCons.Errors.UserError(pkg + ' required to build Abraca')
+conf = env.Configure()
+conf.CheckVala()
+conf.CheckCCompiler()
+conf.CheckPkgConfig()
 
 for pkg in ['gtk+-2.0', 'xmms2-client', 'xmms2-client-glib']:
-	if not conf.CheckPkg(pkg):
-		raise SCons.Errors.UserError(pkg + ' required to build Abraca')
-	env.ParseConfig('pkg-config --libs --cflags ' + pkg)
-	env.Append(VALAPKGS = [pkg])
+	if conf.CheckPkg(pkg):
+		env.ParseConfig('pkg-config --libs --cflags ' + pkg)
+		env.Append(VALAPKGS = [pkg])
 
 conf.Finish()
 
 env.Append(VALAPKGS = ['playlist-map'])
 env.Append(CCFLAGS = ['-Wall', '-Wno-unused-variable'])
 
-if env['debug']:
+if env.DebugVariant():
 	env.Append(CCFLAGS = ['-g'])
 else:
 	env.Append(CCFLAGS = ['-O2'])
---
+++ abracaenv.py
@@ -0,0 +1,82 @@
+import SCons
+from SCons.Script.SConscript import SConsEnvironment
+from SCons.Options import Options, PathOption, BoolOption
+
+class AbracaEnvironment(SConsEnvironment):
+	def __init__(self, *args, **kwargs):
+		SConsEnvironment.__init__(self, *args, **kwargs)
+
+		# Oldest usable SCons version.
+		self.EnsureSConsVersion(0, 97)
+
+		# Load the custom vala builder
+		self.Tool('vala', toolpath=['scons-tools'])
+
+		# Add some build options
+		opts = Options(['.scons_options'])
+		opts.AddOptions(
+			BoolOption('silent', 'build silently', True),
+			BoolOption('debug', 'build debug variant', True),
+			PathOption('prefix', 'install prefix', '/usr/local'),
+		)
+		opts.Update(self)
+		opts.Save('.scons_options', self)
+
+		self.Help(opts.GenerateHelpText(self))
+
+		# Hide compiler command line if silent mode on
+		if self['silent']:
+			self['VALADEFINESCOMSTR'] = '   Defines: $TARGET'
+			self['VALACOMSTR']        = 'Generating: $TARGETS'
+			self['CCCOMSTR']          = '  Building: $TARGET'
+			self['LINKCOMSTR']        = '   Linking: $TARGET'
+
+	def Configure(self):
+		conf = SConsEnvironment.Configure(self, custom_tests = {
+			'CheckPkgConfig' : AbracaEnvironment.CheckPkgConfig,
+			'CheckPkg' : AbracaEnvironment.CheckPkg,
+			'CheckVala' : AbracaEnvironment.CheckVala,
+			'CheckCCompiler' : AbracaEnvironment.CheckCCompiler,
+		})
+		return conf
+
+	def DebugVariant(self):
+		return self['debug']
+
+	def CheckPkgConfig(ctx, fail=True):
+		ctx.Message('Checking for pkg-config... ')
+		exit_code, output = ctx.TryAction('pkg-config --version')
+		ctx.Result(exit_code)
+		if not exit_code and fail:
+			raise SCons.Errors.UserError('The pkg-config tool is required to build')
+		return exit_code
+	CheckPkgConfig = staticmethod(CheckPkgConfig)
+
+	def CheckPkg(ctx, pkg, fail=True):
+		ctx.Message('Checking for %s... ' % pkg)
+		exit_code, output = ctx.TryAction('pkg-config --exists \'%s\'' % pkg)
+		ctx.Result(exit_code)
+		if not exit_code and fail:
+			raise SCons.Errors.UserError('The %s library is required to build' % pkg)
+		return exit_code
+	CheckPkg = staticmethod(CheckPkg)
+
+	def CheckCCompiler(ctx, fail=True):
+		ctx.Message('Checking for compiler... ')
+		code = 'int main (int argc, char **argv) { return 0; }'
+		exit_code = ctx.TryCompile(code, '.c')
+		ctx.Result(exit_code)
+		if not exit_code and fail:
+			raise SCons.Errors.UserError('The c compiler is required to build')
+		return exit_code
+	CheckCCompiler = staticmethod(CheckCCompiler)
+
+	def CheckVala(ctx, fail=True):
+		ctx.Message('Checking for valac... ')
+		code = 'public class Test { public static void main (string[] args) { } }'
+		exit_code = ctx.TryBuild(ctx.env.Vala, code, '.vala')
+		ctx.Result(exit_code)
+		if not exit_code and fail:
+			raise SCons.Errors.UserError('The valac compiler is required to build')
+		return exit_code
+	CheckVala = staticmethod(CheckVala)
