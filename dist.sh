a4f30b2674ca61d66f792db541f4728845077feb
Daniel Svensson <dsvensson@gmail.com>
2010-12-06 20:30:29 +0100

    OTHER: Add release script.

---
+++ dist.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=$(git describe)
+
+echo "Current version: abraca-$VERSION"
+echo -n "Keep? [Y/n] "
+
+read NEW_TAG
+if [ x$NEW_TAG = "xN" ] || [ x$NEW_TAG = "xn" ]; then
+	echo -n "New version: "
+	read VERSION
+	git tag -s -m "Abraca $VERSION release." $VERSION
+fi
+
+git archive --format=tar --prefix=abraca-${VERSION}/ HEAD | gzip -cn9 > ../abraca-${VERSION}.tar.gz
