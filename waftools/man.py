17918fa8b51b24112b7c7a83a23e4eeb65b362e2
Daniel Svensson <dsvensson@gmail.com>
2012-12-10 00:56:55 +0100

    OTHER: Migrate from SCons to waf.

---
+++ waftools/man.py
@@ -0,0 +1,56 @@
+from waflib import Task, Errors, Utils
+import os
+import gzip
+
+from waflib.TaskGen import feature,before_method
+
+def gzip_func(task):
+    infile = task.inputs[0].abspath()
+    outfile = task.outputs[0].abspath()
+
+    input = None
+    outf = None
+    output = None
+    try:
+        input = open(infile, 'rb')
+        outf = open(outfile, 'wb')
+        output = gzip.GzipFile(os.path.basename(infile), fileobj=outf)
+        output.write(input.read())
+    finally:
+        if input:
+            input.close()
+        if output: # Must close before outf to flush compressed data.
+            output.close()
+        if outf:
+            outf.close()
+
+Task.task_factory('man', gzip_func, color='BLUE')
+
+@feature('man')
+@before_method('process_source')
+def process_man(self):
+    source = self.to_nodes(getattr(self, 'source', []))
+    self.source = []
+
+    section = getattr(self, 'section', None)
+
+    for node in source:
+        if not node:
+            raise Errors.BuildError('cannot find input file %s for processing' % x)
+
+        # s = section or node.name.rpartition('.')[2]
+        s = section or "." in node.name and node.name.rsplit(".", 1)[1]
+        if not s:
+            raise Errors.BuildError('cannot determine man section from filename')
+
+        out = self.path.find_or_declare(node.name + '.gz')
+
+        tsk = self.create_task('man')
+        tsk.set_inputs(node)
+        tsk.set_outputs(out)
+
+        self.bld.install_files('${MANDIR}/man%s' % s, out)
+
+
+def configure(conf):
+    return True
---
+++ wscript
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+VERSION = '0.7.1'
+APPNAME = 'Abraca'
+
+top = '.'
+out = 'build'
+
+def options(opt):
+	opt.load('compiler_c vala intltool')
+
+def configure(conf):
+	conf.load('compiler_c vala intltool')
+	conf.load('gresource man', tooldir='waftools')
+
+	conf.check_vala((0, 18, 0))
+
+	conf.check_cfg(package='gio-2.0', atleast_version='2.34.0', args='--cflags --libs')
+	conf.check_cfg(package='gio-unix-2.0', atleast_version='2.34.0', args='--cflags --libs')
+	conf.check_cfg(package='glib-2.0', atleast_version='2.34.0', args='--cflags --libs')
+	conf.check_cfg(package='gmodule-2.0', atleast_version='2.34.0', args='--cflags --libs')
+	conf.check_cfg(package='gtk+-3.0', atleast_version='3.4.0', args='--cflags --libs')
+	conf.check_cfg(package='gee-1.0', atleast_version='0.6', args='--cflags --libs')
+	conf.check_cfg(package='xmms2-client', atleast_version='0.8', args='--cflags --libs')
+	conf.check_cfg(package='xmms2-client-glib', atleast_version='0.8', args='--cflags --libs')
+
+	conf.env.VALADEFINES = []
+
+	if conf.check_cc(function_name='xmmsc_coll_query', header_name='xmmsclient/xmmsclient.h', uselib='XMMS2-CLIENT', mandatory=False):
+		conf.env.VALADEFINES.append('XMMS_API_COLLECTIONS_TWO_DOT_ZERO')
+
+	conf.env.VALADEFINES.append('DEST_OS_' + conf.env.DEST_OS.upper())
+
+	conf.env.DEFINES.append('GETTEXT_PACKAGE=\"Abraca\"')
+
+	conf.define('APPNAME', APPNAME)
+	conf.define('VERSION', VERSION)
+
+	conf.write_config_header('build-config.h')
+
+def build(bld):
+	bld.recurse('external src data po')
